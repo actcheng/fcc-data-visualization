9c12ff8d93404c8c573a935b47f9405489359220
CT Cheng (Anna) <annacheng310@gmail.com>
2019-02-24 22:20:35 +0900

    Axes

--- bar-chart/app.js
+++ bar-chart/app.js
@@ -1,34 +1,63 @@
 d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json").then(function(data){
   dataset = data["data"];
-  const w = '90%';
-  const h = 400;
-  const padding = 0;
+  const w = 700;
+  const h = 500;
+  const padding = 60;
   const color = '#e4aa7c';
+
+  var minDate = new Date(1947,0,1);
+  var maxDate = new Date(2015,6,1);
+
+  var xScale = d3.scaleTime()
+                  .domain([minDate, maxDate])
+                  .range([padding, w-padding]);
   var yScale = d3.scaleLinear()
-                    .range([padding,h - padding])
-                    .domain([0, d3.max(dataset, (d) => d[1])]);
-  const svg = d3.select("body")
+                  .domain([0, d3.max(dataset, (d) => d[1])])
+                  .range([h - 2*padding,0]);
+  const svg = d3.select("#container")
                 .append("svg")
                 .attr("width",w)
                 .attr("height",h)
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisRight(yScale);
+
+  svg.append("g")
+     .attr("id","x-axis")
+     .attr("transform", "translate(0, " + (h-padding) + ")")
+     .call(xAxis);
+
+  svg.append("g")
+     .attr("id","y-axis")
+     .attr("transform", "translate(" +(w-padding) + " ,"+padding+")")
+     .call(yAxis);
+
+
   bars = svg.selectAll("rect")
             .data(dataset)
             .enter()
             .append("rect")
             .attr("class","bar")
 
-  bars.attr("x",(d,i)=>i*90/dataset.length+'%')
-      .attr("y",(d)=>h-yScale(d[1]))
-      .attr("width",90/dataset.length+'%')
-      .attr("height",(d)=>yScale(d[1]))
+
+  bars.attr("x",(d,i)=>padding+i*(w-padding*2)/dataset.length)
+      .attr("y",(d)=>padding+yScale(d[1]))
+      .attr("width",(w-padding*2)/dataset.length)
+      .attr("height",(d)=>h-2*padding-yScale(d[1]))
       .attr("fill", color)
-      .on("mouseover", function() {
-            d3.select(this)
-            	.attr("fill", "red");
-        })
-      .on("mouseout", function() {
-            d3.select(this)
-            	.attr("fill", color);
-        })
-  console.log(dataset[0],dataset[dataset.length-1])
+      .attr("data-date",(d)=>d[0])
+      .attr("data-gdp",(d)=>d[1])
+
+  // d3.select("div#container")
+  //   .append("div")
+  //   .classed("svg-container", true) //container class to make it responsive
+  //   .append("svg")
+  //   //responsive SVG needs these 2 attributes and no width and height attr
+  //   .attr("preserveAspectRatio", "xMinYMin meet")
+  //   .attr("viewBox", "0 0 600 400")
+  //   //class to make it responsive
+  //   .classed("svg-content-responsive", true);
+
+  console.log(dataset[0][0],dataset[dataset.length-1][0])
+  console.log(minDate, maxDate)
 });
