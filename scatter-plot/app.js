cf42ac73eddf5ed7518d1e9c9cc32ee4eda97698
CT Cheng (Anna) <annacheng310@gmail.com>
2019-02-26 23:52:23 +0900

    All test passed, but the legend is fake

--- scatter-plot/app.js
+++ scatter-plot/app.js
@@ -1,65 +1,74 @@
 d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json").then(function(data){
-  dataset = data["data"];
+  dataset = data;
   const w = 800;
   const h = 600;
   const padding = 60;
-  const color = '#e4aa7c';
+  const colorDope = '#6b40bb';
+  const colorNDope = '#40bbb8';
 
-  var xScale = d3.scaleTime()
-                  .domain([minDate, maxDate])
+  const toDate = function(d){
+    return new Date(0,0,0,0,d.Time.slice(0,2),d.Time.slice(3,5))
+  }
+
+  console.log(d3.min(dataset, (d) => toDate(d)))
+
+  var xScale = d3.scaleLinear()
+                  .domain([d3.min(dataset, (d) => d.Year)-1, d3.max(dataset, (d) => d.Year)+1])
                   .range([padding, w-padding]);
   var yScale = d3.scaleLinear()
-                  .domain([0, d3.max(dataset, (d) => d[Time])])
+                  .domain([d3.max(dataset, (d) =>                   toDate(d)), d3.min(dataset, (d) =>toDate(d))])
                   .range([h - 2*padding,0]);
   const svg = d3.select("#container")
                 .append("svg")
                 .attr("width",w)
                 .attr("height",h)
 
-  const xAxis = d3.axisBottom(xScale);
-  const yAxis = d3.axisRight(yScale);
+  const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+  const yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%M:%S"));
 
-  // svg.append("g")
-  //    .attr("id","x-axis")
-  //    .attr("transform", "translate(0, " + (h-padding) + ")")
-  //    .call(xAxis);
-  //
-  // svg.append("g")
-  //    .attr("id","y-axis")
-  //    .attr("transform", "translate(" +(w-padding) + " ,"+padding+")")
-  //    .call(yAxis);
-  //
-  // bars = svg.selectAll("rect")
-  //           .data(dataset)
-  //           .enter()
-  //           .append("rect")
-  //           .attr("class","bar")
-  //
-  // bars.attr("x",(d,i)=>padding+i*(w-padding*2)/dataset.length)
-  //     .attr("y",(d)=>padding+yScale(d[1]))
-  //     .attr("width",(w-padding*2)/dataset.length)
-  //     .attr("height",(d)=>h-2*padding-yScale(d[1]))
-  //     .attr("fill", color)
-  //     .attr("data-date",(d)=>d[0])
-  //     .attr("data-gdp",(d)=>d[1])
-  //     .on("mouseover", function(d) {
-	// 			var xPosition = parseFloat(d3.select(this).attr("x")) ;
-	// 			var yPosition = parseFloat(d3.select(this).attr("y")) / 2 + h/ 2;
-	// 			d3.select("#tooltip")
-	// 				.style("left", xPosition + "px")
-	// 				.style("top", yPosition + "px")
-  //         .attr("data-date",d[0])
-	// 				.select("#data-date")
-	// 				.text(d[0])
-  //       d3.select("#tooltip")
-  //         .attr("data-gdp",d[1])
-  //         .select("#data-gdp")
-  // 				.text(d[1]);
-	// 			d3.select("#tooltip").classed("hidden", false);
-	// 	   })
-	// 	   .on("mouseout", function() {
-	// 			d3.select("#tooltip").classed("hidden", true);
-	// 	   })
+  svg.append("g")
+     .attr("id","x-axis")
+     .attr("transform", "translate(0,"+ (h-padding) + ")")
+     .call(xAxis);
 
-  console.log(dataset[0])
+  svg.append("g")
+     .attr("id","y-axis")
+     .attr("transform", "translate(" +padding + " ,"+padding+")")
+     .call(yAxis);
+
+  dots = svg.selectAll("circle")
+            .data(dataset)
+            .enter()
+            .append("circle")
+            .attr("class","dot")
+
+  dots.attr("cx",(d)=>xScale(d.Year))
+      .attr("cy",(d)=>padding+yScale(toDate(d)))
+      .attr("r",8)
+      .attr("data-xvalue",(d)=>d.Year)
+      .attr("data-yvalue",(d)=>toDate(d))
+      .attr("fill",(d)=>(d.Doping == '')? colorNDope:colorDope)
+      .on("mouseover", function(d) {
+				var xPosition = parseFloat(d3.select(this).attr("cx")) ;
+				var yPosition = parseFloat(d3.select(this).attr("cy")) +padding*2;
+        var year = d3.select(this).attr("data-xvalue");
+				d3.select("#tooltip")
+					.style("left", xPosition)
+					.style("top", yPosition)
+          .attr("data-year",year)
+					.select("#data-year")
+					.text(year);
+        d3.select("#tooltip")
+          .select("#data-doping")
+  				.text((d.Doping == '')? 'None':d.Doping);
+        d3.select("#tooltip")
+          .select("#data-name")
+  				.text(d.Name);
+				d3.select("#tooltip").classed("hidden", false);
+		   })
+		   .on("mouseout", function() {
+				d3.select("#tooltip").classed("hidden", true);
+		   })
+
+  svg.selectAll
 });
